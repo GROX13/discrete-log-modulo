8202dc30f6ce36faf383916f92906249dcd10854
Giorgi <giorgirokhadze@gmail.com>
2016-06-21 21:14:55 +0300

    Done

--- src/me/giorgirokhadze/cryptography/DiscreteLogModuloBase.java
+++ src/me/giorgirokhadze/cryptography/DiscreteLogModuloBase.java
@@ -7,44 +7,42 @@ import static java.lang.Math.*;
 
 abstract class DiscreteLogModuloBase implements DiscreteLogModulo {
 
-	private static final DiscreteLogModulo instance = new DiscreteLogModuloBaseImplementation();
+    private static final DiscreteLogModulo instance = new DiscreteLogModuloBaseImplementation();
 
-	static DiscreteLogModulo getInstance() {
-		return instance;
-	}
+    static DiscreteLogModulo getInstance() {
+        return instance;
+    }
 
-	private static final class DiscreteLogModuloBaseImplementation extends DiscreteLogModuloBase {
+    private static final class DiscreteLogModuloBaseImplementation extends DiscreteLogModuloBase {
 
-		private static final Integer B = (int) pow(2, 20);
+        private static final Integer B = (int) pow(2, 20);
 
-		@Override
-		public String calculate(String p, String g, String h) {
-			BigInteger
-					P = new BigInteger(p),
-					G = new BigInteger(g),
-					H = new BigInteger(h);
-			Hashtable<BigInteger, Integer> leftHandSide = new Hashtable<>();
+        @Override
+        public String calculate(String p, String g, String h) {
+            BigInteger
+                    P = new BigInteger(p),
+                    G = new BigInteger(g),
+                    H = new BigInteger(h);
+            Hashtable<BigInteger, Integer> leftHandSide = new Hashtable<>();
 
-			for (Integer i = 0; i < B; i++) {
-				// BigInteger localResult = H.divide(G.modPow(BigInteger.valueOf(i), P)).mod(P);
-				BigInteger localResult = H.multiply(G.modPow(BigInteger.valueOf(i), P).modInverse(P)).mod(P);
-				
-				leftHandSide.put(localResult, i);				
-			}
-			
-			BigInteger GB = G.modPow(BigInteger.valueOf(B), P);
-			
-			for (Integer i = 0; i < B; i++) {
-				BigInteger localResult = GB.modPow(BigInteger.valueOf(i), P);
-				if (leftHandSide.containsKey(localResult)) {
-					Integer x1 = leftHandSide.get(localResult);
-					return String.valueOf(((long) i) * B + x1);
-				}
-			}
-			
-			return null;
-		}
+            for (Integer i = 0; i < B; i++) {
+                BigInteger localResult = H.multiply(G.modPow(BigInteger.valueOf(i), P).modInverse(P)).mod(P);
+                leftHandSide.put(localResult, i);
+            }
 
-	}
+            BigInteger GB = G.modPow(BigInteger.valueOf(B), P);
+
+            for (Integer i = 0; i < B; i++) {
+                BigInteger localResult = GB.modPow(BigInteger.valueOf(i), P);
+                if (leftHandSide.containsKey(localResult)) {
+                    Integer x1 = leftHandSide.get(localResult);
+                    return String.valueOf(((long) i) * B + x1);
+                }
+            }
+
+            return null;
+        }
+
+    }
 
 }

a48637f0a15871b7345792abd11e487627304e12
Giorgi <giorgirokhadze@gmail.com>
2016-06-21 17:33:07 +0400

    Cleaned up some stuff

--- src/me/giorgirokhadze/cryptography/DiscreteLogModuloBase.java
+++ src/me/giorgirokhadze/cryptography/DiscreteLogModuloBase.java
@@ -25,19 +25,20 @@ abstract class DiscreteLogModuloBase implements DiscreteLogModulo {
 					H = new BigInteger(h);
 			Hashtable<BigInteger, Integer> leftHandSide = new Hashtable<>();
 
-			for (int i = 0; i < B; i++) {
-				BigInteger localResult = H.divide(G.modPow(BigInteger.valueOf(i), P)).mod(P);
+			for (Integer i = 0; i < B; i++) {
+				// BigInteger localResult = H.divide(G.modPow(BigInteger.valueOf(i), P)).mod(P);
+				BigInteger localResult = H.multiply(G.modPow(BigInteger.valueOf(i), P).modInverse(P)).mod(P);
+				
 				leftHandSide.put(localResult, i);				
 			}
 			
 			BigInteger GB = G.modPow(BigInteger.valueOf(B), P);
 			
-			for (int i = 0; i < B; i++) {
+			for (Integer i = 0; i < B; i++) {
 				BigInteger localResult = GB.modPow(BigInteger.valueOf(i), P);
 				if (leftHandSide.containsKey(localResult)) {
 					Integer x1 = leftHandSide.get(localResult);
-					Integer x0 = i;
-					return String.valueOf(x0 * B + x1);
+					return String.valueOf(i * B + x1);
 				}
 			}
 			

c65c40d1600c0312b4d7a2fb22b8fdda930d70b4
Giorgi <giorgirokhadze@gmail.com>
2016-06-21 15:43:14 +0400

    Done, needs to be tested

--- src/me/giorgirokhadze/cryptography/DiscreteLogModuloBase.java
+++ src/me/giorgirokhadze/cryptography/DiscreteLogModuloBase.java
@@ -3,27 +3,47 @@ package me.giorgirokhadze.cryptography;
 import java.math.BigInteger;
 import java.util.Hashtable;
 
+import static java.lang.Math.*;
+
 abstract class DiscreteLogModuloBase implements DiscreteLogModulo {
 
-    private static final DiscreteLogModulo instance = new DiscreteLogModuloBaseImplementation();
+	private static final DiscreteLogModulo instance = new DiscreteLogModuloBaseImplementation();
 
-    static DiscreteLogModulo getInstance() {
-        return instance;
-    }
+	static DiscreteLogModulo getInstance() {
+		return instance;
+	}
 
-    private static final class DiscreteLogModuloBaseImplementation extends DiscreteLogModuloBase {
+	private static final class DiscreteLogModuloBaseImplementation extends DiscreteLogModuloBase {
 
-        @Override
-        public String calculate(String p, String g, String h) {
-            BigInteger P = new BigInteger(p),
-                    G = new BigInteger(g),
-                    H = new BigInteger(h);
-            Hashtable leftHandSide = new Hashtable<>();
-            Integer twoToTheTwenty = (int) Math.pow(2, 20);
+		private static final Integer B = (int) pow(2, 20);
 
-            return null;
-        }
+		@Override
+		public String calculate(String p, String g, String h) {
+			BigInteger
+					P = new BigInteger(p),
+					G = new BigInteger(g),
+					H = new BigInteger(h);
+			Hashtable<BigInteger, Integer> leftHandSide = new Hashtable<>();
 
-    }
+			for (int i = 0; i < B; i++) {
+				BigInteger localResult = H.divide(G.modPow(BigInteger.valueOf(i), P));
+				leftHandSide.put(localResult, i);				
+			}
+			
+			BigInteger GB = G.modPow(BigInteger.valueOf(B), P);
+			
+			for (int i = 0; i < B; i++) {
+				BigInteger localResult = GB.modPow(BigInteger.valueOf(i), P);
+				if (leftHandSide.containsKey(localResult)) {
+					Integer x1 = leftHandSide.get(localResult);
+					Integer x0 = i;
+					return String.valueOf(x0 * B + x1);
+				}
+			}
+			
+			return null;
+		}
+
+	}
 
 }
